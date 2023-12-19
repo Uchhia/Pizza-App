79e9dc858f2b5a70f878d8e4653779f0a4cb6f9b
Rahul Pachauri <rpachauri584@gmail.com>
2023-12-19 17:58:58 +0530

    add:Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,34 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./ui/Home";
+import Menu from "./features/menu/Menu";
+import Order from "./features/order/Order";
+import CreateOrder from "./features/order/CreateOrder";
+import Cart from "./features/cart/Cart";
+const router = createBrowserRouter([
+  {
+    path: "/Pizza-App",
+    element: <Home />,
+  },
+  {
+    path: "/Pizza-App/menu",
+    element: <Menu />,
+  },
+  {
+    path: "/Pizza-App/cart",
+    element: <Cart />,
+  },
+  {
+    path: "/Pizza-App/order/new",
+    element: <CreateOrder />,
+  },
+  {
+    path: "/Pizza-App/order/:orderId",
+    element: <Order />,
+  },
+]);
+
 function App() {
-  // let x = 10;
-  return <>Rahul ye</>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
